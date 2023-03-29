b2cb2e9a8c1cd08954bdb1a56984d3f28c2dd5a6
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-03-29 14:31:36 +0100

    finished translator

--- Pig-Latin-Python.py
+++ Pig-Latin-Python.py
@@ -3,15 +3,17 @@ def translate():
     wordsList = sentence.split()
     vowelList = ['a', 'e' , 'i', 'o', 'u']
     i = 0
-    print(wordsList)
+    pigLatinSentence = []
     while i < len(wordsList):
         currentWord = wordsList[i].lower()
         if currentWord[0] in vowelList:
             currentWord = currentWord + 'ay'
+
         else:
-            
-
-        print(currentWord)
+            letter = currentWord[0]
+            currentWord = currentWord[1:]
+            currentWord = currentWord + '-' + letter + 'ay'
+        pigLatinSentence.append(currentWord)
         i = 1 + i
-
+    print(' '.join(pigLatinSentence))
 translate()

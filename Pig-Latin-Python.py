8986e4ab93b9645fc29121ded91715b47f15153c
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-03-29 14:01:19 +0100

    Handling words starting with vowels

--- Pig-Latin-Python.py
+++ Pig-Latin-Python.py
@@ -1,11 +1,17 @@
 def translate():
     sentence = input('Input your sentence... ')
-    wordsArray = sentence.split()
+    wordsList = sentence.split()
+    vowelList = ['a', 'e' , 'i', 'o', 'u']
     i = 0
-    print(wordsArray)
-    while i < len(wordsArray):
-        currentWord = wordsArray[i]
-        print(currentWord) 
+    print(wordsList)
+    while i < len(wordsList):
+        currentWord = wordsList[i].lower()
+        if currentWord[0] in vowelList:
+            currentWord = currentWord + 'ay'
+        else:
+            
+
+        print(currentWord)
         i = 1 + i
 
 translate()
